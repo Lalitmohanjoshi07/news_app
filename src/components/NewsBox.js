2286d1936f77a668ef9aba3f6a41e0ef6fd6dfbc
Lalit <lalitcse60@gmail.com>
2023-07-01 13:29:00 +0530

    used fetch api to add data

--- src/components/NewsBox.js
+++ src/components/NewsBox.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 
 export class NewsBox extends Component {
   render() {
-    let {title,description}=this.props;
+    let {title,desc,imageLink,url}=this.props;
     return (
-      <div className="card " style={{width: '18rem'}}>
-        <img src="..." className="card-img-top" alt="..."/>
+      <div className="card my-2 " style={{width: '20rem'}}>
+        <img src={imageLink ? imageLink : "https://www.moroylab.org/wp-content/uploads/2018/05/news-2444778_640.jpg"} className="card-img-top" alt="card news"/>
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="/" className="btn btn-small">read more..</a>
+          <p className="card-text">{desc}</p>
+          <a href={url} target="_blank" rel="noreferrer" className="btn btn-small">read more..</a>
         </div>
       </div>
     )
--- src/components/NewsContainer.js
+++ src/components/NewsContainer.js
@@ -7,19 +7,32 @@ export class NewsContainer extends Component {
 
 //   }
 
+  constructor(){
+    super();
+    this.state = {
+      articles: [],
+      loading: true
+    }
+  }
+
+  async componentDidMount(){
+    let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=8fca590d29ea447a93cb07e00913f768&pageSize=100";
+    let data = await fetch(url);
+    let datalog = await data.json();
+    this.setState({articles : datalog.articles})
+    console.log("cdm",datalog)
+  }
   render() {
     return (
       <div className="container my-2">
         <div className="row">
-          <div className="col-md-4">
-            <NewsBox title="hello dosto"/>
-          </div>
-          <div className="col-md-4">
-            <NewsBox title="hello dosto"/>
-          </div>
-          <div className="col-md-4">
-            <NewsBox title="hello dosto"/>
-          </div>
+          {this.state.articles.map((element)=>{
+            return (
+              <div key={element.url} className="col-md-4">
+                <NewsBox title={element.title} imageLink={element.urlToImage} url={element.url} desc={element.description} />
+              </div>
+            )
+          })}
         </div>
       </div>
     )
